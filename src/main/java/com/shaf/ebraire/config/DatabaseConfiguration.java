94bee34ee5cb1e01d4e4ef91f09cf529ee9758df
madoci <36835658+madoci@users.noreply.github.com>
2020-09-08 23:07:49 +0200

    Retire l'option name inexistante

--- src/main/java/com/shaf/ebraire/config/DatabaseConfiguration.java
+++ src/main/java/com/shaf/ebraire/config/DatabaseConfiguration.java
@@ -23,7 +23,7 @@ import java.sql.SQLException;
 @EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
 @EnableTransactionManagement
 @EnableElasticsearchRepositories("com.shaf.ebraire.repository.search")
-@ConditionalOnMissingClass(name = "com.shaf.ebraire.HerokuDatabaseConfiguration")
+@ConditionalOnMissingClass("com.shaf.ebraire.HerokuDatabaseConfiguration")
 public class DatabaseConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
