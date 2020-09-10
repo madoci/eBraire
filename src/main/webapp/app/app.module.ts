c3fc347127487f8b1467e993f68b2fb373d68d2c
LeDrosophile <therve38@gmail.com>
2020-09-10 10:44:26 +0200

    correction total tests front

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -14,7 +14,6 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
-import { DisplayBookComponent } from './display-book/display-book.component';
 import { BookdialogdeleteComponent } from './admin/bookdialogdelete/bookdialogdelete.component';
 
 @NgModule({
