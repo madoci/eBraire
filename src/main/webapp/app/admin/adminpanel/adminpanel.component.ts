dd40816727a032920298217846b4e4ffee3a1e72
LeDrosophile <therve38@gmail.com>
2020-09-10 16:02:51 +0200

    ajout generation auto

--- src/main/webapp/app/admin/adminpanel/adminpanel.component.ts
+++ src/main/webapp/app/admin/adminpanel/adminpanel.component.ts
@@ -1,5 +1,11 @@
+import { TypeService } from './../../entities/type/type.service';
+import { GenreService } from './../../entities/genre/genre.service';
+import { TagService } from './../../entities/tag/tag.service';
+import { IType } from './../../shared/model/type.model';
+import { IGenre } from './../../shared/model/genre.model';
+import { ITag } from './../../shared/model/tag.model';
 import { Component, OnInit } from '@angular/core';
-import { IBook } from '../../shared/model/book.model';
+import { IBook, Book } from '../../shared/model/book.model';
 import { BookService } from '../../entities/book/book.service';
 import { HttpResponse } from '@angular/common/http';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -10,14 +16,68 @@ import { BookdialogdeleteComponent } from '../bookdialogdelete/bookdialogdelete.
   templateUrl: './adminpanel.component.html',
 })
 export class AdminpanelComponent implements OnInit {
+  sending = false;
+  count = 0;
+  numGenerate = 10;
   books: IBook[] = [];
-  constructor(protected modalService: NgbModal, private bookService: BookService) {}
+  types?: IType[];
+  genres?: IGenre[];
+  tags?: ITag[];
+
+  constructor(
+    protected modalService: NgbModal,
+    private bookService: BookService,
+    protected typeService: TypeService,
+    protected genreService: GenreService,
+    protected tagService: TagService
+  ) {}
 
   ngOnInit(): void {
     this.bookService.query().subscribe((res: HttpResponse<IBook[]>) => (this.books = res.body || []));
+    // Genres
+    this.genreService.query().subscribe((res: HttpResponse<IGenre[]>) => {
+      this.genres = res.body || [];
+    });
+    // Tags
+    this.tagService.query().subscribe((res: HttpResponse<ITag[]>) => {
+      this.tags = res.body || [];
+    });
+    // Types
+    this.typeService.query().subscribe((res: HttpResponse<IType[]>) => {
+      this.types = res.body || [];
+    });
   }
   delete(book: IBook): void {
     const modalRef = this.modalService.open(BookdialogdeleteComponent, { size: 'lg', backdrop: 'static' });
     modalRef.componentInstance.book = book;
+  }
+
+  generate(): void {
+    if (this.sending) return;
+    this.sending = true;
+    for (let i = 0; i < this.numGenerate; i++) {
+      const book = new Book();
+      book.title = this.makeid(10);
+      book.authors = this.makeid(10);
+      book.description = this.makeid(10);
+      book.unitPrice = Math.floor(Math.random() * 20);
+      const b = this.books[Math.floor(Math.random() * this.books.length - 1)];
+      book.imageContentType = b.imageContentType;
+      book.image = b.image;
+      book.genres = [this.genres![Math.floor(Math.random() * this.genres!.length)]];
+      book.tags = [this.tags![Math.floor(Math.random() * this.tags!.length)]];
+      book.type = this.types![Math.floor(Math.random() * this.types!.length)];
+      this.bookService.create(book).subscribe(() => {});
+    }
+  }
+
+  makeid(length: number): string {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
   }
 }
