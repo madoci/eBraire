46af4af824c9908d4dcecad94d2b0f7f9665011d
Stephen <stephen.andre@roseautechnologies.com>
2020-09-10 22:39:12 +0200

    Autorise l'accès aux pages sans compte

--- src/main/java/com/shaf/ebraire/config/SecurityConfiguration.java
+++ src/main/java/com/shaf/ebraire/config/SecurityConfiguration.java
@@ -82,7 +82,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
-            .antMatchers("/api/**").authenticated()
+            .antMatchers("/api/**").permitAll()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/info").permitAll()
             .antMatchers("/management/prometheus").permitAll()
@@ -90,7 +90,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
             .httpBasic()
         .and()
-            .apply(securityConfigurerAdapter());
+            .apply(securityConfigurerAdapter())
+        .and()
+            .requiresChannel()
+            .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+            .requiresSecure();
         // @formatter:on
     }
 
