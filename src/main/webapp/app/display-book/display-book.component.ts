1c4904a739bf394289bc510e89300e4acc492331
Stephen <stephen.andre@roseautechnologies.com>
2020-09-10 10:06:19 +0200

    Met le titre du livre comme nom de page web #4

--- src/main/webapp/app/display-book/display-book.component.ts
+++ src/main/webapp/app/display-book/display-book.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { map, flatMap } from 'rxjs/operators';
 import { BookService } from 'app/entities/book/book.service';
 import { HttpResponse } from '@angular/common/http';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'jhi-display-book',
@@ -13,7 +14,7 @@ export class DisplayBookComponent implements OnInit {
   book?: IBook | null;
   imageBlobUrl: String = '';
 
-  constructor(private activatedRoute: ActivatedRoute, private bookService: BookService) {}
+  constructor(private activatedRoute: ActivatedRoute, private bookService: BookService, private titleService: Title) {}
 
   ngOnInit(): void {
     this.activatedRoute.params
@@ -27,6 +28,7 @@ export class DisplayBookComponent implements OnInit {
         map((res: HttpResponse<IBook>) => {
           this.book = res.body;
           this.imageBlobUrl = 'data:' + this.book?.imageContentType + ';base64,' + this.book?.image;
+          this.titleService.setTitle('' + this.book?.title);
         })
       )
       .subscribe();
