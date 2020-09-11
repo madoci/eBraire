e70751ef871bbadf6d51b8b2515855962fd43dfd
LeDrosophile <therve38@gmail.com>
2020-09-11 16:48:56 +0200

    decouplage affichage livre, ajout composant pour afficher une couverture #24

--- src/main/webapp/app/display-book/book-details/book-details.component.ts
+++ src/main/webapp/app/display-book/book-details/book-details.component.ts
@@ -10,22 +10,12 @@ import { Title } from '@angular/platform-browser';
 export class BookDetailsComponent implements OnInit {
   @Input() book: IBook = new Book();
   quantity = 1;
-  imageBlobUrl = '';
 
   numInCart = 0;
-  // Style
-  border = 'solid black';
-  imgWidth = 500;
-  borderWidth = 0;
-  borderHeight = 0;
-  borderSize = '';
-  height = 100;
-  width = 100;
 
   constructor(public shoppingCartService: ShoppingCartService, private titleService: Title) {}
 
   ngOnInit(): void {
-    this.imageBlobUrl = 'data:' + this.book.imageContentType + ';base64,' + this.book.image;
     this.titleService.setTitle('' + this.book.title);
     this.numInCart = this.shoppingCartService.getItems().length;
   }
@@ -33,21 +23,5 @@ export class BookDetailsComponent implements OnInit {
   addToCart(): void {
     this.shoppingCartService.addToCart(this.book, this.quantity);
     this.numInCart = this.shoppingCartService.getItems().length;
-  }
-
-  img(): void {
-    const image = document.getElementById('img') as HTMLImageElement;
-    if (image.naturalHeight > image.naturalWidth) {
-      const coeff = (this.imgWidth * image.naturalWidth) / image.naturalHeight / this.imgWidth;
-      this.height = this.imgWidth;
-      image.style.width = 'auto';
-      this.borderWidth = ((1 - coeff) * this.imgWidth) / 2;
-    } else {
-      const coeff = (this.imgWidth * image.naturalHeight) / image.naturalWidth / this.imgWidth;
-      this.width = this.imgWidth;
-      image.style.height = 'auto';
-      this.borderHeight = ((1 - coeff) * this.imgWidth) / 2;
-    }
-    this.borderSize = Math.floor(this.borderHeight) + 'px ' + Math.floor(this.borderWidth) + 'px';
   }
 }
