9f0c5ae385178031529dd2d6e01e84e588a97297
Thomas <therve38@gmail.com>
2020-09-12 23:38:17 +0200

    Gestion de beaucoup de tags

--- src/main/webapp/app/display-book/book-details/book-details.component.ts
+++ src/main/webapp/app/display-book/book-details/book-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { IBook, Book } from 'app/shared/model/book.model';
 import { ShoppingCartService } from 'app/shopping-cart/shopping-cart.service';
 import { Title } from '@angular/platform-browser';
+import { Tag } from '../../shared/model/tag.model';
 
 @Component({
   selector: 'jhi-book-details',
@@ -9,13 +10,34 @@ import { Title } from '@angular/platform-browser';
 })
 export class BookDetailsComponent implements OnInit {
   @Input() book: IBook = new Book();
+
   quantity = 1;
   numInCart = 0;
+
+  // Style
+  organizedTags?: Tag[][];
+  maxChar = 30;
 
   constructor(public shoppingCartService: ShoppingCartService, private titleService: Title) {}
 
   ngOnInit(): void {
     this.titleService.setTitle('' + this.book.title);
+    this.organizedTags = [[]];
+    if (this.book.tags) {
+      let currentChar = 0;
+      let currentTab = 0;
+      let currentTag = 0;
+      while (currentTag < this.book.tags.length) {
+        this.organizedTags[currentTab].push(this.book.tags[currentTag]);
+        currentChar += this.book.tags[currentTag].tag!.length;
+        if (currentChar > this.maxChar) {
+          currentChar = 0;
+          this.organizedTags.push([]);
+          currentTab++;
+        }
+        currentTag++;
+      }
+    }
   }
 
   addToCart(): void {
